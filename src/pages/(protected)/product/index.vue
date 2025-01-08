a0e60e6ac2f8e9500c71c5053d6ff46c02b00e3b
kazuya-wata-nabe <kazuya.watanabe@sorich.jp>
2025-01-08 18:39:03 +0900

    feat: impl text-input components

--- src/pages/(protected)/product/index.vue
+++ src/pages/(protected)/product/index.vue
@@ -2,12 +2,16 @@
 import { ProductFormSchema } from "~/feature/product/model/product"
 import { useForm } from "~/shared/composables/use-form"
 
-const { defineField } = useForm(ProductFormSchema)
+const { defineField, errors } = useForm(ProductFormSchema)
 const [name] = defineField("name")
 </script>
 
 <template>
   <FlexCol>
-    <input>
+    <TextInput
+      v-model="name"
+    >
+      {{ errors }}
+    </TextInput>
   </FlexCol>
 </template>
---
+++ src/shared/components/text-input.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  required?: boolean
+}>()
+
+const model = defineModel<string | undefined>()
+const id = useId()
+</script>
+
+<template>
+  <label :for="id">
+    <input
+      :id="id"
+      v-model="model"
+    >
+  </label>
+  <slot />
+</template>
